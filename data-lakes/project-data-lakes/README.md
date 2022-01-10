3910ceee1a79c90d4a5fb01e13933019ca2ae3ca
Barbara Jöbstl <barbara.joebstl@mercedes-benz.io>
2022-01-10 17:34:34 +0000

    data lakes assingment: update etl.py, readme

--- data-lakes/project-data-lakes/README.md
+++ data-lakes/project-data-lakes/README.md
@@ -2,6 +2,15 @@
 
 ## About
 
+This is the assignment for the part *Data Lakes with Spark* of the Udacity Nanodegree progamm. 
+The aim of this part is to learn:
+- Why we nee Data Lakes
+- Big data technology effect on data warehousing
+- How to do Data Lakes
+- Big Data on AWS_The Options
+- Data Lake Issues
+
+#### Data introduction - assignment
 A music streaming startup, Sparkify, has grown their user base and song database even more and want to move their data warehouse to a data lake. Their data resides in S3, in a directory of JSON logs on user activity on the app, as well as a directory with JSON metadata on the songs in their app.
 
 As their data engineer, you are tasked with building an ETL pipeline that extracts their data from S3, processes them using Spark, and loads the data back into S3 as a set of dimensional tables. This will allow their analytics team to continue finding insights in what songs their users are listening to.
@@ -9,7 +18,7 @@ As their data engineer, you are tasked with building an ETL pipeline that extrac
 You'll be able to test your database and ETL pipeline by running queries given to you by the analytics team from Sparkify and compare your results with their expected results.
 
 
-## Data Sets
+##### Data Sets
 - Song data: s3://udacity-dend/song_data
 - Log data: s3://udacity-dend/log_data
 
@@ -19,12 +28,13 @@ The first dataset is a subset of real data from the Million Song Dataset. Each f
 song_data/A/B/C/TRABCEI128F424C983.json
 song_data/A/A/B/TRAABJL12903CDCF1A.json
 
+### Assignment
+The task is to build an ETL pipeline for a data lake hosted on S3. We need to load data from S3, process the data into the analytics tables (see image below) using Spark, and load them back into S3. This Spark process will be deployed on a cluster using AWS. 
 
-# TASK: 
-create an etl.py that loads the data transform to tables according to requirements and loads the data back to s3
 
-WIP
-## Run jupyter notebook to develop locally in a docker container
+## Prerequisites
+### Run locally 
+For prototyping an testing it's nice to run Spark locally in a Docker container. Everything you need you'll find in the `local-setup` folder.
 We use a build based on the docker image "jupyter/pyspark-notebook".
 Custom additions:
 - libraries needed, as written in the requirements.txt
@@ -34,14 +44,57 @@ Custom additions:
 
 start container: `docker-compose up`, if you changed something in the config make sure to use `docker-compose up --force-recreate --build` to make sure changes are build
 
-The user "jovian" stands for "a Jupiter-like planet" in the docker env.
+### AWS
+- AWS CLI installed [Help](https://docs.aws.amazon.com/cli/latest/userguide/install-cliv2.html)
+- Set up Access credentials using AWS IAM. Create a User with admin rights and programatic access. Check with `aws iam list-users`
+- Create an EMR Cluster
+  -- `aws emr create-default-roles` to create an EMR_EC2_DefaultRole for your account
+  -- launch a cluter `emr create-cluster --name {your-name} --use-default-roles --release-label emr-5.28.0 --instance-count 3 --applications Name=Spark --ec2-attributes KeyName={yourKey} --instance-type m5.xlarge --instance-count 3`
+  Make sure you have the .pem for your EC2 in place. Don't forget to terminate the cluster once you are done or add the `--auto-terminate` flag, when creating the cluster
+- Create an S3 Bucket for the output data. Make sure the permissions are set correctly
+- Connect to cluster via SSH
+  -- Edit the security groups inbound rules to authorize SSH traffic (port 22) from your computer [Help](https://docs.aws.amazon.com/emr/latest/ManagementGuide/emr-connect-ssh-prereqs.html)
+  -- Verify connection to the Master node: EC2 -> Instances -> your master node -> Connect to instance [Help](https://docs.aws.amazon.com/emr/latest/ManagementGuide/emr-connect-master-node-ssh.html)
 
- see ipynb: done: explore data and create tables
+## ETL Pipeline
+### A: Data Exploration and testing `local_setup\data\assignment_data_lakes.ipynb`. You cann see the data schema, how to transform the data to tables and how to write them to parquet files.
 
-## TODO: 
-- (get data from s3)
-- store back to s3
-- create functions for required etl.py from working notebook code.
-- update readme
+#### Load Data
+- Load the data from S3 with spark.read.json() and have a look at the schemas.
+// TODO Load image
+
+#### Table schemas for analysis
+## TODO add image
+
+
+### B: `etl.py`
+
+In the etl.py you can find the code from the notebook, refactored as a script. To run the script use `python etl.py`
+
+## Additional Info
+
+ 
+- [Docstrings](https://www.python.org/dev/peps/pep-0257/)
+- [Why PEP8](https://realpython.com/python-pep8/)
+- [PEP 8 Styleguide](http://pep8online.com/)
+
+### EMR - Elastic Map Reduce
+
+Since a Spark cluster includes multiple machines, in order to use Spark code on each machine, we would need to download and install Spark and its dependencies. This is a manual process. Elastic Map Reduce is a service offered by AWS that negates the need for you, the user, to go through the manual process of installing Spark and its dependencies for each machine.
+`emr create-cluster --name {your-name} --use-default-roles --release-label emr-5.28.0 --instance-count 3 --applications Name=Spark --ec2-attributes KeyName={yourKey} --instance-type m5.xlarge --instance-count 3 `
+
+Make sure you have the .pem for your EC2 in place.
+
+Make sure to terminate the cluster once you are done or add the `--auto-terminate` flag, when creating the cluster
+
+### Differnces S3 (Simple Storage Service) and HDFS (Hadoop Distributed File System)
+
+AWS S3 is an object storage system that stores the data using key value pairs, namely bucket and key, and HDFS is an actual distributed file system which guarantees fault tolerance. HDFS achieves fault tolerance by having duplicate factors, which means it will duplicate the same files at 3 different nodes across the cluster by default (it can be configured to different numbers of duplication).
+HDFS has usually been installed in on-premise systems, and traditionally have had engineers on-site to maintain and troubleshoot Hadoop Ecosystem, which cost more than having data on cloud. Due to the flexibility of location and reduced cost of maintenance, cloud solutions have been more popular. With extensive services you can use within AWS, S3 has been a more popular choice than HDFS.
+Since AWS S3 is a binary object store, it can store all kinds of format, even images and videos. HDFS will strictly require a certain file format - the popular choices are avro and parquet, which have relatively high compression rate and which makes it useful to store large dataset.
+
+### Parquet
+
+https://www.upsolver.com/blog/apache-parquet-why-use
 
 
--- data-lakes/project-data-lakes/etl.py
+++ data-lakes/project-data-lakes/etl.py
@@ -1,17 +1,49 @@
-import configparser
+from configparser import ConfigParser, NoSectionError, NoOptionError, ParsingError
 from datetime import datetime
 import os
-from pyspark.sql import SparkSession
-from pyspark.sql.functions import udf, col
-from pyspark.sql.functions import year, month, dayofmonth, hour, weekofyear, date_format
+from pyspark.sql import SparkSession, udf
+from pyspark.sql import functions as F
+import logging
 
 
-config = configparser.ConfigParser()
-config.read('dl.cfg')
+def get_profile_credentials_for_udacity_sandbox():
+    """
+    Get's the credentials from a 'dl.cfg' file.
+    You can use this when you are working in the udacity sandbox
+    """
+    config = ConfigParser()
+    config.read('dl.cfg')
 
-os.environ['AWS_ACCESS_KEY_ID']=config['AWS_ACCESS_KEY_ID']
-os.environ['AWS_SECRET_ACCESS_KEY']=config['AWS_SECRET_ACCESS_KEY']
+    os.environ['AWS_ACCESS_KEY_ID']=config['AWS']['AWS_ACCESS_KEY_ID']
+    os.environ['AWS_SECRET_ACCESS_KEY']=config['AWS']['AWS_SECRET_ACCESS_KEY']
 
+def get_aws_credentials(user_name):
+    """
+    Returns aws credentials for a given profile name
+
+    Params: 
+        user_name: username, where we want to get the aws credentials from
+    """
+
+    config = ConfigParser()
+    config.read([os.path.join(os.path.expanduser("~"), '.aws/credentials')])
+    try:
+        aws_access_key_id = config.get(user_name, 'aws_access_key_id')
+        aws_secret_access_key = config.get(user_name, 'aws_secret_access_key')
+    except ParsingError:
+        logging.error('Error parsing the config file for {}.'.format(user_name))
+        raise
+    except(NoSectionError, NoOptionError):
+        try:
+            aws_access_key_id = config.get('default', 'aws_access_key_id')
+            aws_secret_access_key = config.get('default', 'aws_secret_access_key')
+        except (NoSectionError, NoOptionError):
+            logging.error('Unable to find default AWS credentials')
+            raise
+    return aws_access_key_id, aws_secret_access_key
+
+
+aws_access_key_id, aws_secret_access_key = get_aws_credentials('joebsbar')
 
 def create_spark_session():
     spark = SparkSession \
@@ -22,69 +54,146 @@ def create_spark_session():
 
 
 def process_song_data(spark, input_data, output_data):
+    """
+    Takes in song data from a path, creates tables for the given schema and writes them as parquet files to a given path
+
+    Params:
+        spark: the cursor object
+        input_data: path to the data to process (in our case the song data)
+        output_data: path to where the created parquet files will be written to
+    """
+
     # get filepath to song data file
-    song_data = 
+    # example: song_data/A/B/C/TRABCEI128F424C983.json, thats why we know the depth
+    song_data = f'{input_data}/song_data/*/*/*/*.json'
     
     # read song data file
-    df = 
+    df = spark.read.json(song_data)
+    logging.info('---reading song data from s3')
 
     # extract columns to create songs table
-    songs_table = 
+    songs_table = df.select('song_id', 'title', 'artist_id', 'year', 'duration')
     
     # write songs table to parquet files partitioned by year and artist
-    songs_table
+    songs_table.write.parquet(f'{output_data}/songs_table', mode='overwrite', partitiionBy=['year', 'artist_id'])
+    logging.info('---write songs_table to parquet file on {}'.format(output_data))
+
 
     # extract columns to create artists table
-    artists_table = 
+    artists_table = df.select('artist_id', 'artist_name', 'title', 'artist_location', 'artist_latitude', 'artist_longitude').dropDuplicates()
     
     # write artists table to parquet files
-    artists_table
+    artists_table.write.parquet(f'{output_data}/artists_table', mode='overwrite')
+    logging.info('---write artist_table to parquet file on {}'.format(output_data))
 
 
 def process_log_data(spark, input_data, output_data):
+    """
+    Takes in log data from a path, creates tables for the given schema and writes them as parquet files to a given path
+
+    Params:
+        spark: the cursor object
+        input_data: path to the data to process (in our case the song data)
+        output_data: path to where the created parquet files will be written to
+    """
     # get filepath to log data file
-    log_data =
+    log_data = f'{input_data}/log-data/*json'
 
     # read log data file
-    df = 
-    
+    df = spark.read.json(log_data)
+    logging.info('---reading song data from s3')
+
     # filter by actions for song plays
-    df = 
+    df = df.filter(df['page']== 'NextSong')
 
     # extract columns for users table    
-    artists_table = 
+    users_table = df.select('userId', 'firstName', 'lastName', 'gender', 'level').dropDuplicates()
     
     # write users table to parquet files
-    artists_table
+    users_table.writeparquet(f'{output_data}/users_table', mode='overwrite')
+    logging.info('---write users_table to parquet file on {}'.format(output_data))
 
-    # create timestamp column from original timestamp column
-    get_timestamp = udf()
-    df = 
+    # timetable with DF
+    # 
+    # time_table = df.withColumn('start_time', F.from_unixtime(F.col('ts')/1000))
+    # time_table = time_table.select('ts', 'start_time') \
+    #         .withColumn('year', F.year('start_time')) \
+    #         .withColumn('month', F.month('start_time')) \
+    #         .withColumn('week', F.weekofyear('start_time')) \
+    #         .withColumn('weekday', F.dayofweek('start_time')) \
+    #         .withColumn('day', F.dayofyear('start_time')) \
+    #         .withColumn('hour', F.hour('start_time')) \
+
+    # create start_time column from original timestamp column with a user defined function
+    get_timestamp = udf(lambda timestamp: str(int(int(timestamp) / 1000)))
+    # or spark.udf.register('get_timestamp', lambda timestamp: str(int(int(timestamp) / 1000))
+    df = df.withColumn('start_time', get_timestamp(df['ts']))
     
     # create datetime column from original timestamp column
-    get_datetime = udf()
-    df = 
+    get_datetime = udf(lambda timestamp: str(datetime.fromtimestamp(int(timestamp) / 1000 )))
+    df = df.withColumn('datetime', get_datetime(df['ts']))
     
-    # extract columns to create time table
-    time_table = 
+    #variant SQL
+    time_data = spark.sql("""
+       SELECT DISTINCT 
+           datetime as start_time,
+           hour(datetime) AS hour,
+           day(datetime) AS day,
+           weekofyear(datetime) AS week,
+           month(datetime) AS month,
+           year(datetime) AS weekday
+       FROM ts""")
     
     # write time table to parquet files partitioned by year and month
-    time_table
+    time_data.write.parquet('f{output_data}/time_table', mode='overwrite', partitionBy=['year', 'month'])
+    logging.info('---write time_table to parquet file on {}'.format(output_data))
 
     # read in song data to use for songplays table
-    song_df = 
+    song_data = f'{input_data}/song_data/*/*/*/*.json'
+    song_data = spark.read.json(song_data)
+    logging.info('---reading song data')
 
-    # extract columns from joined song and log datasets to create songplays table 
-    songplays_table = 
+    # create temporary views on the table -  in memory, no writing
+    song_data.createOrReplaceTempView('song_data')
+    log_data.createOrReplaceTempView('log_data')
+    time_data.createOrReplaceTempView('time_data')
+
+    # extract columns from joined song and log datasets to create songplays table
+    songplays_table = spark.sql("""SELECT DISTINCT
+                                        l.ts as songplay_id,
+                                        l.ts as start_time,
+                                        l.userId as user_id,
+                                        l.level as level,
+                                        s.song_id as song_id,
+                                        s.artist_id as artist_id,
+                                        l.sessionId as session_id,
+                                        l.location as location,
+                                        l.userAgent as user_agent
+                                    FROM song_data s
+                                    JOIN log_data l
+                                        ON s.artist_name = l.artist 
+                                        AND s.title = l.song
+                                        AND s.duration = s.lenght
+                                    JOIN time_data t
+                                        ON t.ts = l.ts
+                                """).dropDuplicates()
 
     # write songplays table to parquet files partitioned by year and month
-    songplays_table
+    songplays_table.write.parquet(f'{output_data}/songplays_table', mode='overwrite', partition=['year', 'month'])
+    logging.info('---write songplays to parquet file on {}'.format(output_data))
 
 
 def main():
+    """
+    create a spark session,
+    load data from a given path
+    process song and log data 
+    writes new tables to a given output path
+
+    """
     spark = create_spark_session()
     input_data = "s3a://udacity-dend/"
-    output_data = ""
+    output_data = "s3a://udacity-dend-barbara/"
     
     process_song_data(spark, input_data, output_data)    
     process_log_data(spark, input_data, output_data)
