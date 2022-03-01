a0d8d89a31f76bcdf08c93ab628a7b9737ab3cf8
Barbara Jöbstl <barbara.joebstl@mercedes-benz.io>
2022-03-01 13:57:36 +0000

    Capstone: add udf for port mapping

--- capstone-project/etl_capstone.py
+++ capstone-project/etl_capstone.py
@@ -1,61 +1,55 @@
 from pyspark.sql import functions as F
-from pyspark.sql.functions import year, month, to_date
+from pyspark.sql.functions import year, month, to_date, col
 from pyspark.sql.types import StructType, StructField as Fld, StringType as Str, IntegerType as Int, ShortType as Short, DoubleType as Dbl, ByteType as Bt, TimestampType as Tst, FloatType as Flt
 from pyspark.sql.types import DateType
 from pyspark.sql import SparkSession
-from utils import check_number_of_columns, check_number_of_rows, sas_to_timestamp
+from utils import check_number_of_columns, check_number_of_rows, sas_to_timestamp, get_ports, map_city_name_to_port
 
-OUTPUT_DATA_PATH = "tables"
+OUTPUT_PATH = "tables"
 INPUT_DATA_PATH = "data"
 
 def create_spark_session():
     spark = SparkSession.builder().appName("Capstone Project").getOrCreate()
     return spark
 
-def process_immigration_data(spark): 
-    immigration_schema = StructType([
-        Fld("cicid", Short(), False),
-        Fld("arrdate", Str()),
-        Fld("depdate", Str()),
-        Fld("i94cit", Short(), False),
-        Fld("i94res", Short()),
-        Fld("i94port", Str()),
-        Fld("fltno", Str()),
-        Fld("biryear", Short()),
-        Fld("gender", Bt()),
-        Fld("i94visa", Bt())
-    ])
+def process_data(spark):
 
-    immigration_data = f'{INPUT_DATA_PATH}/sas_data' # holding the parquet files
+    df_immigrant = get_df_immigration(spark)
+    
+    create_fact_immigrant(df_immigrant)
+    create_dim_date(df_immigrant)
+    create_dim_city(spark)
+
+
+def get_df_immigration(spark):
+    immigration_data = f'{INPUT_DATA_PATH}/sas_data/' # holding the parquet files
 
     # instead of all 28 columns, we only need 10 for the tables we are going to create
     df_immigration = spark.read.parquet(immigration_data)\
-                    .select('cicid', 'arrdate', 'depdate', 'i94cit', 'i94res', 'i94port', 'fltno', 'biryear', 'gender', 'i94visa')\
-                    .schema(immigration_schema)
+                    .select('cicid', 'arrdate', 'depdate', 'i94cit', 'i94res', 'i94port', 'fltno', 'biryear', 'gender', 'i94visa')
 
     df_immigration = df_immigration.dropDuplicates(['cicid'])
 
     # add timestamps, created with udf
     df_immigration = df_immigration.withColumn('arrival_ts', sas_to_timestamp(df_immigration['arrdate']))
     df_immigration = df_immigration.withColumn('departure_ts', sas_to_timestamp(df_immigration['depdate']))
+    print(df_immigration.head(5))
+    return df_immigration
 
+
+def create_fact_immigrant(df_immigration):
     # create fact table
-    fact_immigration = df_immigration.select('cicid', 'arrival_ts', 'departure_ts', 'i94cit', 'i94res', 'i94port', 'fltno').dropDuplicates()
-    
-    #df_immigration.printSchema()
-    
+    fact_immigration = df_immigration.select(col('cicid').alias('immigrant_id') , 'arrival_ts', 'departure_ts', 'i94cit', 'i94res', col('i94port').alias('city_code'), 'fltno').dropDuplicates()
+        
     #test data
-    check_number_of_rows(df_immigration)
-    check_number_of_columns(df_immigration, 7)
+    check_number_of_rows(fact_immigration)
+    check_number_of_columns(fact_immigration, 7)
     
+    fact_immigration.printSchema()
     #write table
-    fact_immigration.writeparquet(f'{OUTPUT_DATA_PATH}/fact_immigration', mode='overwrite')
-
-    # TODO would be a possible second part in airflow, because dim tables can only be created once the fact table is in place
-    create_dim_date(spark, df_immigration)
-    create_dim_weather(spark)
-    create_dim_city(spark, df_immigration)
-    create_dim_immigrant(spark, df_immigration)
+    fact_immigration.write.parquet(f'{OUTPUT_PATH}/fact_immigration', mode='overwrite')
+    print(f'Info: Immigration fact table written to: {OUTPUT_PATH}/fact_immigration')
+    
 
 def create_dim_date(df_immigration):
     # get all existing timestamps and drop duplicats
@@ -63,6 +57,7 @@ def create_dim_date(df_immigration):
     df_time = df_time.select('arrival_ts').unionAll(df_time.select('departure_ts'))
     df_time = df_time.withColumnRenamed('arrival_ts', 'ts')
     df_time.dropDuplicates()
+    df_time.dropna()
 
     # create dim_time table
     dim_time = df_time.select('ts') \
@@ -74,15 +69,20 @@ def create_dim_date(df_immigration):
                 .withColumn('day', F.dayofyear('ts')) \
                 .withColumn('hour', F.hour('ts'))
     
+    dim_time.printSchema()
+    dim_time.head(10)
     #test data
     check_number_of_rows(dim_time)
     check_number_of_columns(dim_time, 8)
+    print('Success: date dimension data validation')
+
 
     #write table
-    dim_time.writeparquet(f'{OUTPUT_DATA}/dim_time', mode='overwrite', partitionBy=['year', 'month'])
+    dim_time.write.parquet(f'{OUTPUT_PATH}/dim_time', mode='overwrite', partitionBy=['year', 'month'])
+    print(f'Info: date dimension table written to: {OUTPUT_PATH}/dim_time')
+
 
 def create_dim_city(spark):
-
     city_schema = StructType([
         Fld("city_name", Str()),
         Fld("state", Str()),
@@ -95,17 +95,27 @@ def create_dim_city(spark):
     ])
 
     # todo pass in main, so that in can be easily changed
-    fname = 'f{INPUT_DATA_PATH}/us-cities-demographics.csv'
+    fname = f'{INPUT_DATA_PATH}/us-cities-demographics.csv'
     dim_city =  spark.read.option("delimiter", ";").csv(fname, schema=city_schema, header=True)
+
+    get_ports() #set the ports
+    dim_city = dim_city.withColumn("city_code", map_city_name_to_port(dim_city["city_name"]))
+
+    dim_city.printSchema()
 
     #test data
     check_number_of_rows(dim_city)
     check_number_of_columns(dim_city, 8)
-    
+    print('Success: city dimension data validation')
+
+
     #write table
-    dim_city.writeparquet(f'{OUTPUT_DATA_PATH}/dim_city', mode='overwrite')
+    dim_city.write.parquet(f'{OUTPUT_PATH}/dim_city', mode='overwrite')
+    print(f'Info: city dimension table written to: {OUTPUT_PATH}/dim_city')
+
 
 def create_dim_weather(spark):
+    
     weather_schema = StructType([
         Fld("dt", DateType()),
         Fld("AverageTemperature", Flt()),
@@ -116,7 +126,7 @@ def create_dim_weather(spark):
         Fld("Longitude", Int())
     ])
 
-    fname = 'f{INPUT_DATA_PATH}/GlobalLandTemperaturesByCity.csv'
+    fname = f'{INPUT_DATA_PATH}/GlobalLandTemperaturesByCity.csv'
 
     df_temperature = spark.read.csv(fname, schema=weather_schema, header=True)
 
@@ -128,23 +138,33 @@ def create_dim_weather(spark):
     df_temperature_us = df_temperature_us.filter(df_temperature_us.date >= "1970-01-01")
     df_temperature_us = df_temperature_us.withColumn('year', year(df_temperature_us.date))
     df_temperature_us = df_temperature_us.withColumn('month', month(df_temperature_us.date))
+    
+    df_temperature_us = df_temperature_us.withColumn("city_code", map_city_name_to_port(df_temperature_us["City"]))
 
     #test data
     check_number_of_rows(df_temperature_us)
     check_number_of_columns(df_temperature_us, 7)
+    print('Success: temparature dimension data validation')
+
 
     #write data
     df_temperature_us.write.parquet('./dim_temperature', mode='overwrite', partitionBy=['year', 'month'])
+    print(f'Info: temperature dimension table written to: {OUTPUT_PATH}/dim_temperature')
+
         
 def create_dim_immigrant(df_immigration):
+    
     dim_immigrant_person = df_immigration.select('cicid', 'biryear', 'gender', 'i94visa').dropDuplicates()
     
     #test data
     check_number_of_rows(dim_immigrant_person)
     check_number_of_columns(dim_immigrant_person, 4)
-    
+    print('Success: immigrant person dimension data validation')
+
     #write data
-    dim_immigrant_person.writeparquet('f{OUTPUT_DATA}/dim_immigrant_person', mode='overwrite')
+    dim_immigrant_person.write.parquet(f'{OUTPUT_PATH}/dim_immigrant_person', mode='overwrite')
+    print(f'Info: immigrant dimension table written to: {OUTPUT_PATH}/dim_immigrant_person')
+
 
 def main():
     """
@@ -155,7 +175,7 @@ def main():
 
     """
     spark = create_spark_session()    
-    process_immigration_data(spark)    
+    process_data(spark)    
 
 
 if __name__ == "__main__":
--- capstone-project/utils.py
+++ capstone-project/utils.py
@@ -1,25 +1,9 @@
+import re
 from pyspark.sql.functions import udf
 import pandas as pd
 
-def check_number_of_rows(df):
-    """ counts the rows of a given dataframe
 
-    :param df: spark dataframe to check counts on
-    :param table_name: corresponding name of table
-    """
-    number_of_rows = df.count()
-    if  number_of_rows == 0:
-        print(f"Error: There are no records in {df}")
-    else:
-        print(f"Info: {df} contains {number_of_rows}")
-
-def check_number_of_columns(df, expected_colums):
-    number_of_columns = len(df.columns)
-
-    if number_of_columns != expected_colums:
-        print(f"Error: {df} - There are {number_of_columns} instead of {expected_colums}.")
-    else:
-        print(f"Info: {df} contains {number_of_columns}.")
+ports = {}
 
 
 @udf
@@ -37,3 +21,47 @@ def sas_to_timestamp(date_sas: float) -> int:
         datetime = pd.to_timedelta((date_sas), unit='D') + pd.Timestamp('1960-1-1')
         timestamp = datetime.timestamp()
         return timestamp
+
+def get_ports():
+    """
+    creates a list of citys with its code
+    data taken from SAS Description Labels "I94PORT"
+    """ 
+
+    with open("data/I94_SAS_Labels_Descriptions.SAS") as f:
+        lines = f.readlines()
+
+    re_compiled = re.compile(r"\'(.*)\'.*\'(.*)\'")
+    for line in lines[302:961]:
+        results = re_compiled.search(line)
+        ports[results.group(1)] = results.group(2)
+
+
+@udf
+def map_city_name_to_port(city):
+
+    for key in ports:
+        if city.lower() in ports[key].lower():
+            return key
+
+def check_number_of_rows(df):
+    """ counts the rows of a given dataframe
+
+    :param df: spark dataframe to check counts on
+    :param table_name: corresponding name of table
+    """
+    number_of_rows = df.count()
+    if  number_of_rows == 0:
+        print(f"Error: There are no records in {df}")
+    else:
+        print(f"Info: Current DF contains {number_of_rows} rows")
+
+def check_number_of_columns(df, expected_colums):
+    number_of_columns = len(df.columns)
+
+    if number_of_columns != expected_colums:
+        print(f"Error: There are {number_of_columns} instead of {expected_colums} for the current Dataframe")
+    else:
+        print(f"Info: {number_of_columns} columnns for current DF.")
+
+
